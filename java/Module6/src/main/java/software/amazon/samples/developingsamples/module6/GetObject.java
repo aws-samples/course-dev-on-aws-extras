5b04faa082591b67d074d63f8ebcc3c745f98b32
Willis <willismt@3c22fbb201a5.ant.amazon.com>
2022-12-07 14:22:43 -0500

    Adding headobject call to Java example in Module6

--- java/Module6/src/main/java/software/amazon/samples/developingsamples/module6/GetObject.java
+++ java/Module6/src/main/java/software/amazon/samples/developingsamples/module6/GetObject.java
@@ -6,6 +6,8 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -20,7 +22,7 @@ public class GetObject {
                 .region(region)
                 .credentialsProvider(credentialsProvider)
                 .build();
-
+        printHeadObject(s3, "morgansamples3bucket", "airports.csv");
         byte[] objectBytes = getObjectBytes(s3, buildObjectRequest("<bucketName>","<keyName>"));
         writeBytesToFile(objectBytes, "/Documents");
         s3.close();
@@ -34,6 +36,17 @@ public class GetObject {
                 .build();
     }
 
+    private static void printHeadObject(S3Client s3, String bucketName, String key) {
+        HeadObjectRequest headObjectRequest = HeadObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        HeadObjectResponse headObjectResponse = s3.headObject(headObjectRequest);
+
+        System.out.println("Object metadata: " + headObjectResponse.toString());
+    }
+
     private static byte[] getObjectBytes(S3Client s3, GetObjectRequest objectRequest) {
         ResponseBytes<GetObjectResponse> objectBytes = s3.getObjectAsBytes(objectRequest);
         return objectBytes.asByteArray();
@@ -41,7 +54,6 @@ public class GetObject {
 
     private static void writeBytesToFile(byte[] data, String path) {
         try {
-            // Write the data to a local file.
             File myFile = new File(path);
             OutputStream os = new FileOutputStream(myFile);
             os.write(data);
