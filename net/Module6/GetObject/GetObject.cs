f4d6c5e703c264fd1d53ba675df37c50c94efbe8
Morgan Willis <willismt@amazon.com>
2022-12-07 19:22:05 +0000

    Adding GetObjectAttributesAsync call to Module6 .NET code examples

--- net/Module6/GetObject/GetObject.cs
+++ net/Module6/GetObject/GetObject.cs
@@ -11,6 +11,7 @@ public class GetObject
         string keyName = "airports.csv";
 
         IAmazonS3 s3Client = new AmazonS3Client(RegionEndpoint.USEast1);
+        await PrintObjectMetadata(s3Client, bucketName, keyName);
         await ReadObjectDataAsync(s3Client, bucketName, keyName);
     }
 
@@ -34,5 +35,27 @@ public class GetObject
             Console.WriteLine($"Error saving {objectName}: {ex.Message}");
             return false;
         }
+    }
+
+    public static async Task PrintObjectMetadata(IAmazonS3 client, string bucketName, string objectName) {
+        // Read more about the GetObjectAttributesAsync call here:
+        // https://docs.aws.amazon.com/sdkfornet/v3/apidocs/items/S3/MS3GetObjectAttributesAsyncGetObjectAttributesRequestCancellationToken.html 
+        List<ObjectAttributes> objectAttributes = new List<ObjectAttributes>();
+        objectAttributes.Add("StorageClass");
+        objectAttributes.Add("ObjectSize");
+
+        GetObjectAttributesRequest request = new GetObjectAttributesRequest{
+            BucketName = bucketName,
+            Key = objectName,
+            ObjectAttributes = objectAttributes
+        };
+
+        CancellationToken token = new CancellationToken();
+
+        GetObjectAttributesResponse response = await client.GetObjectAttributesAsync(request, token);
+
+        Console.WriteLine("Object storage class: " + response.StorageClass + " , Object size: " + response.ObjectSize);
+
+        
     }
 }
